c224ac829ffa9711f6a0a84c28430168a540dcb2
Ham5t3r <anton.taleiko@gmail.com>
2023-05-30 15:02:52 +0300

    Emphasized that one of the steps will generate a large output file in the documentation

--- WP4/sumo-hki-cm/completePipeline.sh
+++ WP4/sumo-hki-cm/completePipeline.sh
@@ -72,6 +72,7 @@ duarouter -c sumo_files/duarcfg_file.trips2routes.duarcfg --write-trips true --i
 python3 tools/randomDepartureTimes.py sumo_files/verified_trips.rou.xml
 python3 tools/departureTimeSorter.py
 python3 tools/indexZeroToN.py
+# NOTE: Will generate a 33 GB vehicle trajectory output file if the output is set in the .sumocfg file!
 python3 sumo_files/runner.py
 
 # Reduction
