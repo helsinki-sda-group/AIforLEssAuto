9d460a6da74b68df7b5a405f98481afbefe18200
nomadflamingo <tetrenkuchioan@gmail.com>
2024-06-04 17:21:30 +0300

    removed unnecessary vectorized env

--- WP4/rl-ridepooling/sumo_rl_rs/environment/__init__.py
+++ WP4/rl-ridepooling/sumo_rl_rs/environment/__init__.py
@@ -8,10 +8,3 @@ register(
     entry_point="sumo_rl_rs.environment.env:SumoEnvironment",
     kwargs={"single_agent": True},
 )
-
-
-register(
-    id="vec-sumo-rl-rs-v0",
-    entry_point="sumo_rl_rs.environment.vectorized_env:VectorizedSumoEnvironment",
-    kwargs={"num_envs": 8, "single_agent": True},
-)
