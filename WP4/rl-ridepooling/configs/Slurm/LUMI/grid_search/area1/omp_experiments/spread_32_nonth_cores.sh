b292c20a09116022443b4c18b3c84bd30542cc70
nomadflamingo <volodymyr.beimuk@helsinki.fi>
2024-10-15 22:38:06 +0300

    Even more LUMI scalability configs

---
+++ WP4/rl-ridepooling/configs/Slurm/LUMI/grid_search/area1/omp_experiments/spread_32_nonth_cores.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#SBATCH --job-name="spread_32_nonthr_cores"
+#SBATCH --output="output/%A_%a-%x-stdout.log"
+#SBATCH --error="output/%A_%a-%x-stderr.log"
+#SBATCH --account=project_462000655
+#SBATCH --time=2:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=20G
+#SBATCH --partition=small
+
+# load modules
+module --force purge
+module load LUMI/23.09
+module load lumi-CPEtools
+module load partition/C
+module load PyTorch/2.2.0-rocm-5.6.1-python-3.10-singularity-exampleVenv-20240315
+
+# declare env variables for SUMO
+export LIBSUMO_AS_TRACI=1
+export SUMO_HOME=/user-software/venv/pytorch/bin
+
+# declare env variables for OMP
+export OMP_DISPLAY_ENV=verbose
+
+#export OMP_NUM_THREADS=${SRUN_CPUS_PER_TASK}
+echo ${OMP_NUM_THREADS}
+export OMP_PROC_BIND=spread
+export OMP_PLACES=cores
+
+cd ~/prj/AIforLEssAuto/WP4/rl-ridepooling
+pwd
+
+taskset -cp $$
+srun hybrid_check -n -r
+
+srun singularity exec --bind /usr/lib64 $SIF python src/tests/gym_test-rs.py --config configs/policy_training/helsinki_updated_areas/area1_sampled_0.2.yaml --num-envs 32 --postfix ${SLURM_JOB_ID}_${SLURM_JOB_NAME}
+
+
---
+++ WP4/rl-ridepooling/configs/Slurm/LUMI/grid_search/area3/0.8_sampled/3_0.8_256e_128c.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#SBATCH --job-name="3_0.8_256e_128c"
+#SBATCH --output="output/%A_%a-%x-stdout.log"
+#SBATCH --error="output/%A_%a-%x-stderr.log"
+#SBATCH --account=project_462000655
+#SBATCH --time=8:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=128
+#SBATCH --mem=256G
+#SBATCH --partition=small
+
+# load modules
+module --force purge
+module load LUMI/24.03
+module load lumi-CPEtools
+module load partition/C
+module load PyTorch/2.2.2-rocm-5.6.1-python-3.10-vllm-0.4.0.post1-singularity-20240617
+
+# list all modules
+module list
+
+# declare env variables for SUMO
+export LIBSUMO_AS_TRACI=1
+export SUMO_HOME=/user-software/venv/pytorch/bin
+
+# declare env variables for OMP
+#export OMP_DISPLAY_ENV=verbose
+
+cd ~/prj/AIforLEssAuto/WP4/rl-ridepooling
+pwd
+
+taskset -cp $$
+srun hybrid_check -n -r
+
+srun --hint=compute_bound singularity exec --bind /usr/lib64 $SIF python src/tests/gym_test-rs.py --config configs/policy_training/helsinki_updated_areas/area3_sampled_0.8.yaml --total-iters 256 --num-envs 256 --postfix ${SLURM_JOB_ID}_${SLURM_JOB_NAME}
+
+
