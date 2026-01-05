8418457ff452d06cb902fe8bcd6d673946decedf
Volodymyr <volodymyr.beimuk@helsinki.fi>
2026-01-05 17:40:34 +0200

    add 126 env bash file (not tested)

---
+++ WP4/rl-ridepooling/configs/Slurm/MAHTI/multi_core/126env.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#SBATCH --job-name="126env"
+#SBATCH --output="slurm_output/%A_%a-%x-stdout.log"
+#SBATCH --error="slurm_output/%A_%a-%x-stderr.log"
+#SBATCH --account=project_2016787
+#SBATCH --time=2:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --partition=medium
+#SBATCH --contiguous
+
+# Mahti: 1 CPU core = 1.875 GiB memory (auto-allocated)
+# 128 cores = ~33.75 GiB memory (126 SUMO envs + main process + buffer)
+
+# load modules
+module load pytorch
+
+# List all modules
+module list
+
+# declare env variables for SUMO
+export LIBSUMO_AS_TRACI=1
+export SUMO_HOME="/projappl/project_2016787/AIforLEssAuto/WP4/rl-ridepooling/ridepool-venv/bin"
+
+cd /projappl/project_2016787/AIforLEssAuto/WP4/rl-ridepooling
+pwd
+
+taskset -cp $$
+
+srun echo $OMP_NUM_THREADS
+
+srun singularity exec \
+    -B "/usr/lib64/libnsl.so.1" \
+    -B /run/nvme \
+    -B /users/volodymy \
+    "$SING_IMAGE" \
+    bash -c "source ridepool-venv/bin/activate && \
+    python src/tests/gym_test-rs.py \
+    --config configs/policy_training/old_net/default.yaml \
+    --num-envs 126 \
+    --postfix ${SLURM_JOB_ID}_${SLURM_JOB_NAME}"
